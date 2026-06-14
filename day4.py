422cd6774fcaef0725b24a239357be3084940060
Dishitha0607 <dishi0607@gmail.com>
2026-06-14 09:55:26 +0530

    day4 complete

---
+++ day4.py
@@ -0,0 +1,17 @@
+def user_logic(n, s):
+    return ''.join(sorted(s,key=lambda ch : ((ord(ch) - 96) %5 , -ord(ch))))
+
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().strip().split()
+    
+    n = int(data[0])  # First input is the integer n
+    s = data[1]  # Second input is the string
+    
+    # Call user logic function and print the output
+    result = user_logic(n, s)
+    print(result)
+
+if __name__ == "__main__":
+    main()
